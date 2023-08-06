552ba5776f7ae2f97254a99cb7d5c602a14aa5d1
erobakiewicz <eliasz.robakiewicz@netguru.com>
2023-08-06 14:38:09 +0200

    prompts

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import openai
 import os
 
+from IPython.display import display, HTML
 from dotenv import load_dotenv, find_dotenv
+
 _ = load_dotenv(find_dotenv())
 
-openai.api_key  = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=0.1,  # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
+
+
+prompt = f"""
+I want to build bookshelf for my room. The room is 3 meters wide and 4 meters long. I want it to be able to hold
+5000 books. What are possible designs for the bookshelf? What materials should I use? What tools do I need? How much
+does it cost in Poland (prices in PLN). 
+Create a list of 5 possible and different from each other designs in length and width of shelf and in arrangement e.g.
+in the corner, across the whole room. Create short summary of the each design (less than 100 words) with pros and cons.
+
+"""
+response = get_completion(prompt)
+print(response)
+# display(HTML(response))
---
+++ prompt_examples.py
@@ -0,0 +1,20 @@
+prompts = {
+    "marxist_economics": """
+List 10 best marxist books on economics and list of 10 books of neoclassical economics. Compare both lists, look for 
+common topics and significant differences.
+
+Step 1: List 10 best marxist books on economics with a short description of each.
+Step 2: Create second list of 10 books most commonly read with the books from step 1 (10 for each book).
+Step 3: Create a short summary (less than 100 words) of what books from list 1 and books from list 2 have in common
+and what are the differences between them. To do this, you have to compare each element of list 1 with each element of
+of list 2. In summary no specific book should be mentioned, only general topics and differences between them.
+""",
+    "bookshelf": """
+I want to build bookshelf for my room. The room is 3 meters wide and 4 meters long. I want it to be able to hold
+5000 books. What are possible designs for the bookshelf? What materials should I use? What tools do I need? How much
+does it cost in Poland (prices in PLN). 
+Create a list of 5 possible and different from each other designs in length and width of shelf and in arrangement e.g.
+in the corner, across the whole room. Create short summary of the each design (less than 100 words) with pros and cons.
+
+"""
+}
