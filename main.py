cd384cc88fa3749e332511c85b3f88b5d44ad305
erobakiewicz <eliasz.robakiewicz@netguru.com>
2023-08-07 15:27:46 +0200

    new prompt for iterables

--- main.py
+++ main.py
@@ -9,12 +9,25 @@ _ = load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+pre_prompt = "Check grammar and correct mistakes in following text: \n"
+
+
 def get_prompt():
     key = input("Please provide a key for test prompt: ")
     if test_prompt := prompts.get(key):
         additional_info = input("Do you want to provide any additional info? ")
-        value = f"{additional_info} \n\n {test_prompt}"
+        value = f"{pre_prompt}{additional_info} \n\n {test_prompt}"
         return value
+
+
+# use prompt for each object through iterable
+def get_multi_prompt(seq):
+    if isinstance(seq, dict):
+        iterable_item = ' '.join(f'{k}: {v} \n' for k, v in seq.items())
+    else:
+        iterable_item = ' '.join(f'{num + 1}: {val}' for num, val in enumerate(seq))
+    additional_info = input("Do you want to provide any additional info? ")
+    return f"{pre_prompt} {additional_info} \n\n {iterable_item}"
 
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
@@ -27,5 +40,9 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
+# regular prompt
 response = get_completion(get_prompt())
+# iterable prompt
+# response = get_completion(get_multi_prompt(seq=prompts.get("Espanol")))
+
 print(response)
